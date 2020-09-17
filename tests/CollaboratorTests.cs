385483f44631fdb4083d5ffb49c3a5a92f90a4e4
Joost Schriek <give@joost.coffee>
2020-09-17 12:00:49 -0400

    removing the loggy output thing

--- tests/CollaboratorTests.cs
+++ tests/CollaboratorTests.cs
@@ -1,34 +1,22 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
 using Xunit;
 using gitman;
 using Octokit;
-using Moq;
-using System.Threading.Tasks;
-using Xunit.Abstractions;
+
 
 namespace Tests
 {
     public class CollaboratorTests
     {
-        public CollaboratorTests(ITestOutputHelper output) 
-        {
-            Console.SetOut(new Loggy(output));
-        }
-
         public class Exclusion : CollaboratorTests {
             IEnumerable<string> repos = new List<string>{ "portal", "agent", "stuff" };
             IEnumerable<GitTeam> existingTeamsOnRpos = new [] { "bravo", "charlie", "delta" }.Select(t => new GitTeam(1, t));
             
-            public Exclusion(ITestOutputHelper output) : base(output) { }
-
             public class OnlyTests : Exclusion {
                 Collaborators collab =  new Collaborators(null, "Alpha", only: new [] { "portal" });
                 
-                public OnlyTests(ITestOutputHelper output) : base(output) { }
-
                 [Fact]
                 public void should_do_nothing_with_notportal() 
                 {
@@ -63,8 +51,6 @@ namespace Tests
         
             public  class NotTests : Exclusion {
                 Collaborators collab =  new Collaborators(null, "Alpha", not: new [] { "portal" });
-
-                public NotTests(ITestOutputHelper output) : base(output) { }
 
                 [Fact]
                 public void should_not_add_portal() {
