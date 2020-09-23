58e6bb848acf13e72dc4c2f372f1f177a8fa10c9
Joost Schriek <give@joost.coffee>
2020-09-23 09:59:16 -0400

    fixing the protections check
    adding the right format for env vars in the launch json
    
    fixing the unsetting of a protection check
    
    do not crash everything if you can't find the branch
    
    adding more logging to protection unset
    
    maybe doubling up like this work better
    
    fuck octokit

--- src/Protection.cs
+++ src/Protection.cs
@@ -1,20 +1,25 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Linq;
 using Octokit;
 
 namespace gitman
 {
     public class Protection : BaseBranchAction
     {
+        private readonly IReadOnlyList<string> EmptyContexts;
         private int reviewers;
         private string branch;
         private const string UPDATE = "[UPDATE] ";
         private IList<string> cachedReviewers = new List<string>();
         private Dictionary<string, BranchProtectionRequiredStatusChecks> cachedStatusContexts = new Dictionary<string, BranchProtectionRequiredStatusChecks>();
 
+        
         public Protection(string branch = "master", int reviewers = 2)
         {
+            EmptyContexts = new List<string>().AsReadOnly();
+
             this.reviewers = reviewers;
             this.branch = branch;
         }
@@ -24,58 +29,69 @@ namespace gitman
             BranchProtectionSettings requiredReviewers = null;
             var message = UPDATE;
             
-                try {
-                    var statusChecks = await Client.Repository.Branch.GetRequiredStatusChecks(repo.Owner.Login, repo.Name, this.branch);
-                    if (statusChecks.Strict) 
-                    {
-                        message += "will remove strict reviewers";
-                        this.cachedStatusContexts.Add(repo.Name, statusChecks);
-                    }
-                } catch (Octokit.NotFoundException) { 
-                    // no-op -- we didn't find any restrictions so that is good.            
+            try {
+                var statusChecks = await Client.Repository.Branch.GetRequiredStatusChecks(repo.Owner.Login, repo.Name, this.branch);
+                if (statusChecks.Strict) 
+                {
+                    message += "will remove strict reviewers";
+                    this.cachedStatusContexts.Add(repo.Name, statusChecks);
                 }
+            } catch (Octokit.NotFoundException) { 
+                // no-op -- we didn't find any restrictions so that is good. 
+            }
 
+            var doesNotHaveRequiredReviewers = true;
+            try {
                 requiredReviewers = await Client.Repository.Branch.GetBranchProtection(repo.Owner.Login, repo.Name, this.branch);
-                var doesNotHaveRequiredReviewers = requiredReviewers?.RequiredPullRequestReviews == null || requiredReviewers.RequiredPullRequestReviews.RequiredApprovingReviewCount < reviewers;
-                if (doesNotHaveRequiredReviewers)
-                {
-                    if (!message.Equals(UPDATE)) message += " and ";
-                    message += $"will add {reviewers} review enforcement";
-                    this.cachedReviewers.Add(repo.Name);
-                }
+                doesNotHaveRequiredReviewers = requiredReviewers?.RequiredPullRequestReviews == null || requiredReviewers.RequiredPullRequestReviews.RequiredApprovingReviewCount < reviewers;
+            } catch (Octokit.NotFoundException) {
+                // no-op -- this usually means that it's a new repo
+            }
+            
+            if (doesNotHaveRequiredReviewers)
+            {
+                if (!message.Equals(UPDATE)) message += " and ";
+                message += $"will add {reviewers} review enforcement";
+                this.cachedReviewers.Add(repo.Name);
+            }
 
-                if (message.Equals(UPDATE))
-                {
-                    l($"[OK] {repo.Name} already has {this.branch} branch protection with the number of reviewers and non-strict", 1);
-                }
-                else
-                {
-                    l($"{message} on {repo.Name}", 1);
-                    all_repos.Add(repo);
-                }
+            if (message.Equals(UPDATE))
+            {
+                l($"[OK] {repo.Name} already has {this.branch} branch protection with the number of reviewers and non-strict", 1);
+            }
+            else
+            {
+                l($"{message} on {repo.Name}", 1);
+                all_repos.Add(repo);
+            }
         }
 
         public override async Task Action(Repository repo)
         {
-            BranchProtectionSettingsUpdate update;
-            BranchProtectionRequiredReviewsUpdate reviewersEnforcement = null;
-            BranchProtectionRequiredStatusChecksUpdate notSoStrict = null;
+            try {
+                BranchProtectionRequiredStatusChecks statusChecks;
+                BranchProtectionRequiredStatusChecksUpdate statusChecksUpdate;
 
-            if (cachedReviewers.Contains(repo.Name)) {
-                reviewersEnforcement = new BranchProtectionRequiredReviewsUpdate(false, false, reviewers);
-            }
-            if (cachedStatusContexts.TryGetValue(repo.Name, out var statusChecks)) {
-                notSoStrict = new BranchProtectionRequiredStatusChecksUpdate(false, statusChecks.Contexts);
-            }
-            
-            update = new BranchProtectionSettingsUpdate(
-                requiredStatusChecks: notSoStrict,
-                requiredPullRequestReviews: reviewersEnforcement,
-                false
-            );
+                if (cachedStatusContexts.TryGetValue(repo.Name, out statusChecks)) {
+                    statusChecksUpdate = new BranchProtectionRequiredStatusChecksUpdate(false, statusChecks.Contexts);
+                } else {
+                    statusChecksUpdate = new BranchProtectionRequiredStatusChecksUpdate(false, EmptyContexts);
+                }
 
-            var prot = await Client.Repository.Branch.UpdateBranchProtection(repo.Owner.Login, repo.Name, this.branch, update);
-            l($"[MODIFIED] {repo.Name} is set to {reviewers} reviewers and not strict", 1);
+                l($"[MODIFING] Setting branch protections on {repo.Name} to unstrict and with contexts {string.Join(",", statusChecksUpdate.Contexts)} ", 1);
+                await Client.Repository.Branch.UpdateBranchProtection(
+                    repo.Owner.Login,
+                    repo.Name, 
+                    this.branch, 
+                    new BranchProtectionSettingsUpdate(
+                        statusChecksUpdate,
+                        new BranchProtectionRequiredReviewsUpdate(false, false, reviewers),
+                        false
+                    )
+                );
+            } catch (Octokit.NotFoundException) {
+                l($"[WARN] could not set anything on {repo.Name} because {this.branch} does not exist.", 1);
+            }
         }
     }
 }
